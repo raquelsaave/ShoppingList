29cddc396b7faf1c509ccb4e4c3205600f58e584
Raquel Saavedra <raquel.saavedra@unosquare.com>
2019-08-29 16:56:16 -0500

    Template con mustache y una variable de vista

--- shopping-list/assets/js/index2.js
+++ shopping-list/assets/js/index2.js
@@ -1,57 +1,3 @@
-// let textInput = document.getElementById('text');
-// let addButton = document.getElementById('add');
-// let list = document.getElementById('list');
-// let saveButton = document.getElementById('save');
-// let id = 1;
-// let attributeNumber = 0;
-// let form = document.getElementById('form')
-
-
-// function onEditItem(event) {
-//     let item = event.target.closest('li');
-//     let textSpan = item.firstElementChild
-//     textInput.value = textSpan.textContent;
-//     attributeNumber = textSpan.getAttribute('id')
-//     form.setAttribute('class', 'edit')
-// }
-
-
-// function onDeleteItem(event) {
-//     let item = event.target.closest('li');
-//     list.removeChild(item);
-// }
-
-// function createElement(tag, text, className, events) {
-//     let element = document.createElement(tag);
-//     element.setAttribute('class', className);
-//     element.innerHTML = text;
-//     for (let eventName in events) {
-//         element[eventName] = events[eventName];
-//     }
-
-//     return element;
-// }
-
-// addButton.onclick = function () {
-//     let newLi = createElement('li', `<span id="${id}"> ${textInput.value} </span>`, 'list-item');
-//     newLi.appendChild(createElement('a', 'Edit', 'btn btn-edit', { onclick: onEditItem }));
-//     newLi.appendChild(createElement('a', 'Delete', 'btn btn-delete', { onclick: onDeleteItem }));
-//     list.appendChild(newLi);
-//     textInput.value = '';
-//     id = id + 1;
-// }
-
-
-// saveButton.onclick = function () {
-//     let el = document.getElementById(`${attributeNumber}`)
-//     var text = textInput.value;
-//     el.innerHTML = text;
-//     textInput.value = '';
-//     form.setAttribute('class', 'new')
-// }
-
-
-
 function ShoppingList(root) {
     this.root = root,
     this.id = 1,
@@ -70,10 +16,21 @@ function ShoppingList(root) {
 
 
 ShoppingList.prototype.add = function () {
-    let newLi = createElement('li', `<span id="${this.id}"> ${this.textInput.value} </span>`, 'list-item');
-    newLi.appendChild(createElement('a', 'Edit', 'btn btn-edit', { onclick: this.onEditItem }));
-    newLi.appendChild(createElement('a', 'Delete', 'btn btn-delete', { onclick: this.onDeleteItem }));
-    this.list.appendChild(newLi);
+
+    // Interpolar vista y modelo
+    let itemTemplate = '<li> <span id="{{id}}"> {{text}} </span> <a class="btn btn-edit">Edit</a> <a class="btn btn-delete">Delete</a></li>';
+    let listItem = Mustache.render(itemTemplate, { id: this.id, text: this.textInput.value })
+   
+    // Crear nodo
+    let template = document.createElement('template');
+    template.innerHTML = listItem;
+
+    // Insertar
+    this.list.appendChild(template.content);
+
+    // Binding de botones 
+    this.list.querySelector('.btn-edit').onclick = this.onEdit.bind(this)
+    this.list.querySelector('.btn-delete').onclick = this.onDelete.bind(this)
     this.textInput.value = '';
     this.id = this.id + 1;
 }
@@ -99,19 +56,21 @@ ShoppingList.prototype.onDelete = function () {
     this.list.removeChild(item);
 }
 
-function createElement(tag, text, className, events) {
-    let element = document.createElement(tag);
-    element.setAttribute('class', className);
-    element.innerHTML = text;
-    for (let eventName in events) {
-        element[eventName] = events[eventName];
-    }
+// function createElement(tag, text, className, events) {
+//     let element = document.createElement(tag);
+//     element.setAttribute('class', className);
+//     element.innerHTML = text;
+//     for (let eventName in events) {
+//         element[eventName] = events[eventName];
+//     }
 
-    return element;
+//     return element;
+// }
+
+let allLists = document.querySelectorAll('.shopping-list')
+console.log(allLists)
+for (let i = 0; i < allLists.length; i++) {
+    new ShoppingList(allLists[i])   
 }
 
-let allLists= document.querySelectorAll('.shopping-list')
 
-for (let i=0; i<allLists.length; i++) {
-    new ShoppingList(allLists[i])
-}
